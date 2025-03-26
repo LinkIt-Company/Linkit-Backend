632cdee92e5f2e18216ed6c52ac643c648d24254
hye-on <ain0103@naver.com>
2025-03-26 14:25:44 +0900

    chore: 리뷰 반영. api 랑 일치하지 않는 스웨거 문서 수정

--- src/modules/classification/docs/patchAIPost.docs.ts
+++ src/modules/classification/docs/patchAIPost.docs.ts
@@ -1,14 +1,11 @@
 import { applyDecorators } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { AIPostListResponse } from '../response/ai-post-list.dto';
 
 export const PatchAIPostDocs = applyDecorators(
   ApiOperation({
     summary: 'Post 하나 이동',
     description: '추천해준 폴더로 post이동',
   }),
-  ApiResponse({
-    type: AIPostListResponse,
-  }),
+  ApiResponse({}),
   ApiBearerAuth(),
 );
