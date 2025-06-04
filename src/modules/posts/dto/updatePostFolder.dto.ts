542ac67634a5fc00db9d2e0a2358e52b3ae11842
Anjonghun <58875626+JonghunAn@users.noreply.github.com>
2025-06-04 21:32:01 +0900

    refactor: mongo id 검증로직 제거

--- src/modules/posts/dto/updatePostFolder.dto.ts
+++ src/modules/posts/dto/updatePostFolder.dto.ts
@@ -1,9 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsMongoId, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class UpdatePostFolderDto {
   @IsNotEmpty()
-  @IsMongoId()
   @ApiProperty()
   folderId: string;
 }
